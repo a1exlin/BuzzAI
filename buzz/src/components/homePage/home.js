1bd7f2ea9665864cf9300b1be9315b923059cc7e
Alex Lin <alexlin@Alexs-MacBook-Pro.local>
2025-01-14 20:45:13 -0500

    added functional react-app

---
+++ buzz/src/components/homePage/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Home(){
+    return(
+        <div>
+            <header>BuzzAI here, how can I help?</header>
+            
+        </div>
+        
+    );
+}
+
+export default Home;
